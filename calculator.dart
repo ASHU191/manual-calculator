9281efb51ec15c0b9b146a6bcbd31cd22cab3697
ARSALAN AFTAB <106240658+ASHU191@users.noreply.github.com>
2024-05-28 16:59:52 +0500

    Add files via upload

---
+++ calculator.dart
@@ -0,0 +1,15 @@
+void main() {
+  int num1 = 88;
+  String operator = "+";
+  int num2 = 3;
+
+  if (operator == "+") {
+    print(num1 + num2);
+  } else if (operator == "-") {
+    print(num1 - num2);
+  } else if (operator == "/") {
+    print(num1 / num2);
+  } else if (operator == "*") {
+    print(num1 * num2);
+  }
+}
